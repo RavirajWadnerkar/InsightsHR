56341c1071f008e3635f686fe957a5d703da2a75
Raviraj Wadnerkar <ravirajwadnerkar@gmail.com>
2023-12-01 04:21:59 -0800

    Starter Files for the project pickup

---
+++ src/pages/Holidays.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Page from "../components/Page";
+
+const Holidays = () => {
+  return (
+    <Page title="ClockIn Chaos - Holidays">
+      <div>Coming soon...</div>
+    </Page>
+  );
+};
+
+export default Holidays;
